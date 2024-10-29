9ff2e3f1d97f014df929c8a2e35a77c8efeb84da
Borsoni <thiago.borsoni@gmail.com>
2024-10-29 11:31:02 -0300

    feat: criando as pastas iniciais e alguns arquivos

---
+++ Projeto-Backend-IQuirium/Repository/ProjetoBackendIQuiriumContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Projeto_Backend_IQuirium.Repository
+{
+    public class ProjetoBackendIQuiriumContext : DbContext
+    {
+        public ProjetoBackendIQuiriumContext(DbContextOptions<ProjetoBackendIQuiriumContext> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TodoMapping());
+            modelBuilder.ApplyConfiguration(new FabricanteMapping());
+            modelBuilder.ApplyConfiguration(new VeiculoMapping());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
